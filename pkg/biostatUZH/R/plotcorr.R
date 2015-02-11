9eefa75e19678713a7c97c26d1f3b24ab10e7cb8
wastl <wastl@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-02-11 23:56:29 +0000

    modify plotcorr() to show ellipses in the lower triangle and numbers above
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/ebuzh@13 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/biostatUZH/R/plotcorr.R
+++ pkg/biostatUZH/R/plotcorr.R
@@ -3,12 +3,15 @@
 ### Free software under the terms of the GNU General Public License (version 2
 ### or later) a copy of which is available at http://www.R-project.org/Licenses
 ###
-### Copyright (C) 2013 Duncan Murdoch and E. D. Chow
+### A fork of ellipse::plotcorr() by Duncan Murdoch
+###
+### Copyright (C) 2015 Sebastian Meyer
 ################################################################################
 
 "plotcorr" <-
-  function (corr, outline = TRUE, col = 'grey', numbers = FALSE, type = c("full","lower","upper"),
-            diag = (type == "full"), bty = "n", axes = FALSE, xlab = "", ylab = "", asp = 1,
+  function (corr, outline = TRUE, col = 'grey',
+            lower.panel = "ellipse", upper.panel = "number", diag.panel = NULL,
+            bty = "n", axes = FALSE, xlab = "", ylab = "", asp = 1,
             cex.lab = par("cex.lab"), cex = 0.75*par("cex"), mar = 0.1 + c(2,2,4,2), ...)
 {
     savepar <- par(pty = "s", mar = mar)
@@ -32,28 +35,21 @@
     rowlabs <- as.character(rowlabs)
     collabs <- as.character(collabs)
 
-    col <- rep(col, length = length(corr))
+    col <- rep(col, length.out = length(corr))
     dim(col) <- dim(corr)
 
-    type <- match.arg(type)
+    if (!is.null(lower.panel)) {
+        lower.panel <- match.arg(lower.panel, choices = c("ellipse", "number"))
+    }
+    if (!is.null(upper.panel)) {
+        upper.panel <- match.arg(upper.panel, choices = c("ellipse", "number"))
+    }
+    if (!is.null(diag.panel)) {
+        diag.panel <- match.arg(diag.panel, choices = c("ellipse", "number"))
+    }
     
     cols <- 1:coldim
     rows <- 1:rowdim
-    
-    xshift <- 0
-    yshift <- 0
-    
-    if (!diag) {
-      if (type == "upper") {
-	cols <- 2:coldim
-	rows <- 1:(rowdim - 1)
-	xshift <- 1
-      } else if (type == "lower") {
-        cols <- 1:(coldim-1)
-        rows <- 2:rowdim
-        yshift <- -1
-      }
-    }
     
     maxdim <- max(length(rows), length(cols))
 
@@ -67,36 +63,31 @@
 	 type = "n", bty = bty, axes = axes, xlab = "", ylab = "", asp = asp, 
 	 cex.lab = cex.lab, ...)
     text(rep(0, length(rows)), length(rows):1, labels = rowlabs[rows], adj = 1, cex = cex.lab)
-    text(cols-xshift, rep(length(rows) + 1, length(cols)), labels = collabs[cols], 
+    text(cols, rep(length(rows) + 1, length(cols)), labels = collabs[cols], 
 	 srt = 90, adj = 0, cex = cex.lab)
     mtext(xlab,1,0)
     mtext(ylab,2,0) 
     mat <- diag(c(1, 1))
     plotcorrInternal <- function()
     {
-      if (i == j && !diag) return()
-      if (!numbers) {
+      panel <- if (i == j) diag.panel else if (i > j) lower.panel else upper.panel
+      if (is.null(panel)) return()
+      if (panel == "ellipse") {
         mat[1, 2] <- corr[i, j]
         mat[2, 1] <- mat[1, 2]
-        ell <- ellipse(mat, t = 0.43)
-        ell[, 1] <- ell[, 1] + j - xshift
-        ell[, 2] <- ell[, 2] + length(rows) + 1 - i - yshift
+        ell <- ellipse::ellipse(mat, t = 0.43)
+        ell[, 1] <- ell[, 1] + j
+        ell[, 2] <- ell[, 2] + length(rows) + 1 - i
         polygon(ell, col = col[i, j])
         if (outline) lines(ell)
       } else {
-        text(j + 0.3 - xshift, length(rows) + 1 - i - yshift, round(10 * corr[i, j], 0),
+        text(j + 0.3, length(rows) + 1 - i, round(10 * corr[i, j], 0),
              adj = 1, cex = cex)
       }
     }
     for (i in 1:dim(corr)[1]) {
       for (j in 1:dim(corr)[2]) {
-        if (type == "full") {
           plotcorrInternal()
-        } else if (type == "lower" && (i >= j)) {
-          plotcorrInternal()
-        } else if (type == "upper" && (i <= j)) {
-          plotcorrInternal()
-        }
       }
     }
     invisible()
